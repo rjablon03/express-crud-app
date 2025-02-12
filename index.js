9e217a4a87c8b36926f475f6588e0bbee9fe3cab
rjablon03 <secrets_crasser.0l@icloud.com>
2025-02-12 13:15:14 -0500

    Setup vehicles route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => res.send('<h1>Hello!</h1><p>This is the root route for the CIS-371 CRUD Assignment!</p>'));
+
+app.get('/vehicles', (req, res) => res.sendFile(path.join(__dirname, 'views', 'test.html')));
 
 app.listen(port, () => console.log(`CRUD assignment server listening on port ${port}`));
